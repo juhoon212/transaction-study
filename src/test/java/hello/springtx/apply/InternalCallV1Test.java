dea0231e64b72384ffeb3870911c6c1d36c85095
juhoon212 <ws5501@naver.com>
2023-08-12 23:49:48 +0900

    transaction init + aop problem

---
+++ src/test/java/hello/springtx/apply/InternalCallV1Test.java
@@ -0,0 +1,91 @@
+package hello.springtx.apply;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class InternalCallV1Test {
+
+    @Autowired
+    CallService callService;
+
+    @Test
+    void printProxy() {
+        log.info("callService class={}", callService.getClass());
+    }
+
+    @Test
+    void externalCallV2() {
+        callService.external(); // external 호출하면 @Transactional을 호출한게 아니므로 프록시 호출 x
+    }
+
+    @TestConfiguration
+    static class InternalCallV1TestConfig {
+
+        @Bean
+        CallService callService() {
+            return new CallService(internalService());
+        }
+
+        @Bean
+        InternalService internalService() {
+            return new InternalService();
+        }
+    }
+
+    @Slf4j
+    @RequiredArgsConstructor
+    static class CallService {
+
+        private final InternalService internalService;
+
+        public void external() {
+          log.info("call external");
+          printTxInfo();
+          internalService.internal();
+        }
+
+        @Transactional // => Transactional 은 public 에서만 동작 + 비즈니스 로직의 시작점에서 건다.
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+            boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+            log.info("tx readOnly={}", readOnly);
+        }
+    }
+
+    static class InternalService {
+
+        @Transactional
+        public void internal() {
+            log.info("cell internal");
+            printTxInfo();
+        }
+
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+            boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+            log.info("tx readOnly={}", readOnly);
+        }
+
+    }
+
+
+
+
+}
